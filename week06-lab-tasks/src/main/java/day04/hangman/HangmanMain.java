44f83145a7875f16f3c9c2ea2752db32cedb050a
lkeleti <keleti.kaszlo@vipmail.hu>
2021-11-18 19:59:24 +0100

    day04 exercise half done

--- week06-lab-tasks/src/main/java/day04/hangman/HangmanMain.java
+++ week06-lab-tasks/src/main/java/day04/hangman/HangmanMain.java
@@ -1,23 +1,41 @@
-package hangman;
+package day04.hangman;
 
 import java.util.Scanner;
 
 public class HangmanMain {
-
     public static void main(String[] args) {
         new HangmanMain().run();
     }
 
     private void run() {
+        BusinessModel businessModel = new BusinessModel("szarvasmarha", 8);
+        Scanner scanner = new Scanner(System.in);
+        do {
+            printStatus(businessModel);
+            String guess = inputGuess(scanner);
+            boolean success = businessModel.takeGuess(guess);
+            printResult(success, "Jó!", "Rossz!");
+        }
+        while(!(businessModel.won()) && businessModel.hasMoreChance());
+
+        printResult(businessModel.won(), "Ügyes!", "Vesztettél!");
     }
 
-    private String updateWordFound(String wordToFind, String status, String guess) {
-        char[] chars = status.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (wordToFind.charAt(i) == guess.charAt(0)) {
-                chars[i] = guess.charAt(0);
-            }
+    private void printResult(boolean won, String s, String s2) {
+        if (won) {
+            System.out.println(s);
+        } else {
+            System.out.println(s2);
         }
-        return new String(chars);
+    }
+
+    private void printStatus(BusinessModel businessModel) {
+        System.out.println("Itt tartasz: " + businessModel.getWordFound());
+        System.out.println("Ennyi tipped van még: " + businessModel.getChances());
+    }
+
+    private String inputGuess(Scanner scanner) {
+        System.out.print("Mi a következő tipped? ");
+        return scanner.nextLine();
     }
 }
