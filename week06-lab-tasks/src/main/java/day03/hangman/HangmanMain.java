a3c94a1269f4081249cd2c153369a760cbfe2690
lkeleti <keleti.laszlo@vipmail.hu>
2021-11-18 15:53:38 +0100

    day04 hangman init

---
+++ week06-lab-tasks/src/main/java/day03/hangman/HangmanMain.java
@@ -0,0 +1,23 @@
+package hangman;
+
+import java.util.Scanner;
+
+public class HangmanMain {
+
+    public static void main(String[] args) {
+        new HangmanMain().run();
+    }
+
+    private void run() {
+    }
+
+    private String updateWordFound(String wordToFind, String status, String guess) {
+        char[] chars = status.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (wordToFind.charAt(i) == guess.charAt(0)) {
+                chars[i] = guess.charAt(0);
+            }
+        }
+        return new String(chars);
+    }
+}
